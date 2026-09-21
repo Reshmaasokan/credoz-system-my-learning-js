3966904e00b1255fddbbf6541d69fa2cf57ac567
Reshmaasokan <reshma27072022@gmail.com>
2026-09-21 16:27:13 +0530

    updates

---
+++ Operators/AssignmentOperator.js
@@ -0,0 +1,12 @@
+let a=5
+let b=10
+console.log("Before addition",a)
+console.log("Before subtraction",b)
+a+=5
+b-=5
+console.log("After addition",a)
+console.log("After subtraction",b)
+a*=2
+b/=1
+console.log("After multiplication",a)
+console.log("After division",b)
---
+++ datatype/multipleObject.js
@@ -0,0 +1,75 @@
+function time(){
+ const student=[
+    {
+        name:"Reshma",
+        age:22,
+        address:
+        {
+        temp:"2nd cross st, 4th ave west chennai",
+        parmanent : "Shivan kovil st, Tanjore"
+        },        
+        isWorking : true,
+        salary : 52454.25,
+        skillset : ["Java","python","react","angular"]
+    },
+    {
+        name:"Nisha",
+        age : "25",
+        location : "chennai",
+        address: 
+        {
+            temp : "2nd cross st, 4th ave west chennai ",
+            parmanent : "Shivan kovil st, Tanjore"
+        },
+        isWorking : true,
+        salary : 30002.25,
+        skillset : ["html","css","bs"]
+
+    },
+
+    {
+        name:"kumar",
+        age : "25",
+        location : "chennai",
+        address: {
+            temp : "2nd cross st, 4th ave west chennai ",
+            parmanent : "Shivan kovil st, Tanjore"
+        },
+        isWorking : true,
+        salary : 30002.25,
+        skillset : ["html","css","bs",".net","devops"]
+
+    },
+
+    {
+        name:"kavi",
+        age : "25",
+        location : "chennai",
+        address: {
+            temp : "2nd cross st, 4th ave west chennai ",
+            parmanent : "Shivan kovil st, Tanjore"
+        },
+        isWorking : true,
+        salary : 30002.25,
+        skillset : ["html","css","bs"]
+
+    },
+    
+    {
+        name:"Nike",
+        age : "25",
+        location : "chennai",
+        address: {
+            temp : "2nd cross st, 4th ave west chennai ",
+            parmanent : "Shivan kovil st, Tanjore"
+        },
+        isWorking : true,
+        salary : 30002.25,
+        skillset : ["html","css","bs",".net","devops"]
+
+    }
+ ]
+ console.log(student[1].skillset,student[1].name)
+}
+
+time()
