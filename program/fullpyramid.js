3f8f3cba56b7a3a7f05d61d3206a9b6d66f48b51
Reshmaasokan <reshma27072022@gmail.com>
2026-09-23 20:28:15 +0530

    update

---
+++ program/fullpyramid.js
@@ -0,0 +1,25 @@
+function fullpyramid(){
+    for(let i=1;i<5;i++){
+        let row=""
+        for(let j=1;j<=5-i;j++){
+            row+=" "
+        }
+        for(let j=1;j<=i*2-1;j++){
+            row+="*"
+        }
+        console.log(row)
+    }
+    for(let i=3;i>=1;i--){
+        let row=""
+        for(let j=1;j<=5-i;j++){
+            row+=" "
+        }
+        for(let j=1;j<=i*2-1;j++){
+            row+="*"
+        }
+        console.log(row)
+    }
+    
+
+}
+fullpyramid()
---
+++ program/invertedpyramid.js
@@ -0,0 +1,13 @@
+function invertpyra(){
+    for(let i=5;i>=1;i--){
+        let row=""
+        for(let j=1;j<=5-i;j++){
+            row+=" "
+        }
+        for(let j=1;j<=2*i-1;j++){
+            row+="*"
+        }
+        console.log(row)
+    }
+}
+invertpyra()
